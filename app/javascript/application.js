9d3dca130ccda38f072c3015590cc6e19559e0c0
Janaka Vithanage <jkvithana@gmail.com>
2023-01-07 16:28:58 +1100

    Login/signup forms, navbar, footer added.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,13 +1,14 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
-import consumer from './channels/consumer'
-import CableReady from "cable_ready"
+import "@hotwired/turbo-rails";
+import "./controllers";
+
+import consumer from "./channels/consumer";
+import CableReady from "cable_ready";
 import mrujs from "mrujs";
-import { CableCar } from "mrujs/plugins"
+import { CableCar } from "mrujs/plugins";
 
 mrujs.start({
-  plugins: [
-    new CableCar(CableReady)
-  ]
-})
+  plugins: [new CableCar(CableReady)],
+});
+
+CableReady.initialize({ consumer });
